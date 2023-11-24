f337dfd1be81de34f520ef8cad0735d03f44c383
perebonilado <reradiri@eminenttechnology.com>
2023-11-24 09:04:08 +0100

    redux provider registered

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
-import type { AppProps } from 'next/app'
-import "../config/tailwind/main.css"
-import "../config/tailwind/chrome-bug.css"
+import type { AppProps } from "next/app";
+import "../config/tailwind/main.css";
+import "../config/tailwind/chrome-bug.css";
+import { reduxStore } from "../config/redux-store";
+import { Provider as ReduxProvider } from "react-redux";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ReduxProvider store={reduxStore}>
+      <Component {...pageProps} />
+    </ReduxProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
